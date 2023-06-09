029a4d4c039a821bcd60379014ddd47c10223745
Facundo Sander <Facundo@nad.local>
2023-06-09 14:47:50 -0300

    update

---
+++ AT-2backup.py
@@ -0,0 +1,102 @@
+import streamlit as st
+st.set_page_config(
+    page_title="AutoComplete",
+    page_icon="🦓",
+)
+st.title("AutoComplete")
+
+tab = st.sidebar.selectbox("Elige una opción", ["Inalámbricos", "Cableados"])
+
+departamentos_y_ciudades = {
+    "Artigas": ["Artigas", "Bella Unión", "Tomás Gomensoro"],
+    "Canelones": ["Canelones", "Las Piedras", "Pando", "La Paz", "Santa Lucía"],
+    "Cerro Largo": ["Melo", "Río Branco"],
+    "Colonia": ["Colonia Del Sacramento", "Juan Lacaze", "Nueva Helvecia"],
+    "Durazno": ["Durazno", "Villa del Carmen", "Sarandí del Yí"],
+    "Flores": ["Trinidad"],
+    "Florida": ["Florida", "Sarandí Grande"],
+    "Lavalleja": ["Minas"],
+    "Maldonado": ["Maldonado", "Punta del Este", "San Carlos"],
+    "Montevideo": ["Montevideo"],
+    "Paysandú": ["Paysandú"],
+    "Río Negro": ["Fray Bentos", "Young"],
+    "Rivera": ["Rivera"],
+    "Rocha": ["Rocha", "Chuy", "Lascano"],
+    "Salto": ["Salto", "Daymán", "Arapey"],
+    "San José": ["San José de Mayo"],
+    "Soriano": ["Mercedes", "Dolores"],
+    "Tacuarembó": ["Tacuarembó", "Paso de los Toros"],
+    "Treinta y Tres": ["Treinta y Tres"]
+}
+
+with st.expander("Datos Cliente"):
+    hab, rut_col, btn_col = st.columns([1, 1, 1])
+    habilitacion = hab.checkbox("Requiere habilitacion")
+    empresa = rut_col.text_input("Empresa:")
+    rut = btn_col.text_input("rut")
+    x, tel = st.columns([1,1])
+
+    contacto = x.text_input("Contacto:")
+    telefono = tel.text_input("Telefono:")
+    correo_electronico = st.text_input("Correo electrónico:") 
+
+with st.expander("Direccion"):
+    cols4 = st.columns(2)
+    direccion = st.text_input("Calle, número, Barrio:")
+    departamento = cols4[0].selectbox("Departamento:", list(departamentos_y_ciudades.keys()))
+    ciudad = cols4[1].selectbox("Ciudad:", departamentos_y_ciudades[departamento])
+
+# Cambiar 'Serie:' para aceptar formato xxx-xxx-xxx
+if tab == "Inalámbricos":
+    with st.expander("Datos POS (Inalámbrico)"):
+        mod, ser, ter = st.columns([1,1,1])
+        modelo = mod.selectbox("Modelo:", ["V240M-3G", "V240M-WIFI", "VX680-3G", "VX680-WIFI", "V400m-3G", "V400m-WIFI"])
+        serie = ser.text_input("Serie:")
+        terminal = ter.text_input("Terminal:")
+        x, y, z = st.columns([1,1,1])
+        x.divider()
+        y.divider()
+        z.divider()
+        env, por, comp = st.columns([1,1,1])
+        modelo_enviar = env.selectbox("Modelo a enviar:", ["V240M-3G", "V240M-WIFI", "V400m-3G", "V400m-WIFI", "V200T Eth", "VX820 USB", "P400 USB", "VX820 Eth", "P400 Eth"])
+        portador = por.selectbox("Portador:", ["Express", "Combustible"])
+        if '3G' in modelo_enviar:
+            compania_chip = comp.selectbox("Compañía de chip:", ["ANTEL", "CLARO", "MOVISTAR"]) 
+        else:
+            compania_chip = comp.selectbox("Compañía de chip:", [])
+        remp , con , empty = st.columns([1,1,1])
+        remplazar = remp.checkbox("Remplazar")
+        con_fuente = con.checkbox("Con fuente")
+        detalle_problema = st.text_area("Detalle del problema:")
+    if st.button("Cargar Información Inalámbrico"):
+        output = f"""#Datos Cliente \r\nEmpresa: {empresa} \r\nRut: {rut} \r\nContacto: {contacto}, {telefono}, {correo_electronico}  \r\nRequiere habilitacion: {"SI" if habilitacion is True else "NO"} \r\n \r\n#Datos Envio: \r\nDirección: {direccion}, {departamento}, {ciudad} \r\n \r\n#Datos pos (caso inalambrico): \r\nPos: {modelo}, S/N:{serie}, {terminal} \r\nModelo a enviar: {modelo_enviar}, {portador}, {"" if compania_chip is None else f"Operadora: {compania_chip}"} \r\nRemplazar: {"SI" if remplazar is True else "NO"}, Fuente: {"SI" if con_fuente is True else "NO"} \r\nDetalle del problema: {detalle_problema} \r\n
+"""
+
+        st.text_area("Información cargada", value=output, height=250)
+        #st.success("Información copiada al portapapeles.")
+        st.balloons()
+
+elif tab == "Cableados":
+    with st.expander("Datos POS (Cableado)"):
+        mod, ser, ter = st.columns([1,1,1])
+        modelo = mod.selectbox("Modelo:", [ "P400 USB", "VX820 USB", "P400 Eth" "VX820 Eth"])
+        serie = ser.text_input("Serie:")
+        terminal = ter.text_input("Terminal:")
+        d1, d2, d3 = st.columns([1,1,1])
+        d1.divider()
+        d2.divider()
+        d3.divider()
+        env, remp, x = st.columns([1,1,1])
+        modelo_enviar = env.selectbox("Modelo a enviar:", [ "P400 USB", "VX820 USB", "P400 Eth" "VX820 Eth", "V240M-3G", "V240M-WIFI", "V400m-3G", "V400m-WIFI", "V200T Eth"])
+        remp.markdown("&#32;")
+        remp.markdown("&#32;")
+        remp.markdown("&#32;")
+        remplazar = remp.checkbox("Remplazar")
+        detalle_problema = st.text_area("Detalle del problema:")
+
+    if st.button("Cargar Información Cableado"):
+        output = f"""#Datos Cliente \r\nEmpresa: {empresa} \r\nRut: {rut} \r\nContacto: {contacto}, {telefono}, {correo_electronico} \r\nRequiere habilitacion: {"SI" if habilitacion is True else "NO"} \r\n \r\n#Datos Envio: \r\nDirrcción: {direccion}, {departamento}, {ciudad} \r\n \r\n#Datos pos (caso cableado): \r\nPos: {modelo}, S/N:{serie}, {terminal} \r\nModelo a enviar: {modelo_enviar} \r\nRemplazar: {"SI" if remplazar is True else "NO"} \r\nDetalle del problema: {detalle_problema}
+"""
+        st.text_area("Información cargada", value=output, height=250)
+        #st.success("Información copiada al portapapeles.")
+        st.balloons()
